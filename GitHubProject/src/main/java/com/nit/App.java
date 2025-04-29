837876db02c12e7b1a48af6ca2e6c7e31e748aaf
PrakashUngarala <prakashnaidu788@gmail.com>
2025-04-29 16:20:24 +0530

    another new line added

--- GitHubProject/src/main/java/com/nit/App.java
+++ GitHubProject/src/main/java/com/nit/App.java
@@ -10,5 +10,6 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("Project created succesfully");  
+        System.out.println("Another line saved");
     }
 }
