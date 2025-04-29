ef3fa2c0a5d52f4f309e396d0e4e5492d2c5a4db
PrakashUngarala <prakashnaidu788@gmail.com>
2025-04-29 16:14:12 +0530

    new created by tl

---
+++ GitHubProject/src/main/java/com/nit/App.java
@@ -0,0 +1,14 @@
+package com.nit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Project created succesfully");  
+    }
+}
