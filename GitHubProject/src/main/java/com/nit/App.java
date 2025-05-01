f404220f6a991815122ce4c3373e320b9903a225
PrakashUngarala <prakashnaidu788@gmail.com>
2025-05-01 16:17:09 +0530

    dev added m1 method

--- GitHubProject/src/main/java/com/nit/App.java
+++ GitHubProject/src/main/java/com/nit/App.java
@@ -11,5 +11,10 @@ public class App
         System.out.println( "Hello World!" );
         System.out.println("Project created succesfully");  
         System.out.println("Another line saved");
+        System.out.println("Developer changed this line");
+    }
+    public void m1() {
+    		
+    	 System.out.println("Developer added this method");
     }
 }
