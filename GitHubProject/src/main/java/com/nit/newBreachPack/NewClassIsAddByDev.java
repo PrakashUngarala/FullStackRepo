092ebb24dc6119452916260651e989bf0afd6979
PrakashUngarala <prakashnaidu788@gmail.com>
2025-12-13 19:06:56 +0530

    basic stabilized code is created by the eclipse-workspace

--- GitHubProject/src/main/java/com/nit/newBreachPack/NewClassIsAddByDev.java
+++ GitHubProject/src/main/java/com/nit/newBreachPack/NewClassIsAddByDev.java
@@ -4,5 +4,9 @@ public class NewClassIsAddByDev {
 
 	public void demo() {
 		System.out.println("this new class is added by dev");
+		
+	}
+	public void m1() {
+		System.out.println("basic stabialized project is created by the eclipse-workspace");
 	}
 }
